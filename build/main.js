453c78695f2cdf2a2e16fec38b3d6f49064e7483
fuzelx <fuzelxr@gmail.com>
2024-08-28 13:50:03 +0530

    first commit

---
+++ build/main.js
@@ -0,0 +1,12 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function() {
+        document.getElementById('explore').style.display = 'block'; // Show the button
+    }, 3000); // 3000 milliseconds = 3 seconds
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function() {
+        document.querySelector('.loading-container').style.display = 'none';
+    }, 3000); // 3000 milliseconds = 3 seconds
+});
